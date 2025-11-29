de0909c1c6cfca4c66ac2341452fba639b9c5932
selliermatthias15-lgtm <sellier.matthias15@gmail.com>
2025-11-29 12:19:19 +0100

    Update Data_meh DataFrame column names

--- streamlit_app.py
+++ streamlit_app.py
@@ -283,8 +283,8 @@ with col2:
     st.dataframe(model_df, use_container_width=True)
     
     Data_meh = pd.DataFrame({
-        "Colonne 1": [k_w, k_w * 2],
-        "Colonne 2": [AB2[0], AB2[-1]],
+        "Facteur k": [k_w, k_w * 2],
+        "Schmilblick": [AB2[0], AB2[-1]],
         })
     st.dataframe(Data_meh, use_container_width=True)
     
